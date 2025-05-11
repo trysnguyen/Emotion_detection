b24ee9f2a05661e4b50f26650c8ef34af8c1980d
trysnguyen <122746846+trysnguyen@users.noreply.github.com>
2025-05-11 23:50:03 +0700

    Update load_and_process.py
    mới thêm multi-threading cho cái camera đỡ lag

--- load_and_process.py
+++ load_and_process.py
@@ -1,25 +1,29 @@
 import pandas as pd
 import cv2
 import numpy as np
-
+from concurrent.futures import ThreadPoolExecutor
 
 dataset_path = 'fer2013/fer2013/fer2013.csv'
-image_size=(48,48)
+image_size = (48, 48)
+
+def process_pixel_sequence(pixel_sequence):
+    width, height = 48, 48
+    face = [int(pixel) for pixel in pixel_sequence.split(' ')]
+    face = np.asarray(face).reshape(width, height)
+    face = cv2.resize(face.astype('uint8'), image_size)
+    return face.astype('float32')
 
 def load_fer2013():
-        data = pd.read_csv(dataset_path)
-        pixels = data['pixels'].tolist()
-        width, height = 48, 48
-        faces = []
-        for pixel_sequence in pixels:
-            face = [int(pixel) for pixel in pixel_sequence.split(' ')]
-            face = np.asarray(face).reshape(width, height)
-            face = cv2.resize(face.astype('uint8'),image_size)
-            faces.append(face.astype('float32'))
-        faces = np.asarray(faces)
-        faces = np.expand_dims(faces, -1)
-        emotions = pd.get_dummies(data['emotion']).as_matrix()
-        return faces, emotions
+    data = pd.read_csv(dataset_path)
+    pixels = data['pixels'].tolist()
+
+    with ThreadPoolExecutor() as executor:
+        faces = list(executor.map(process_pixel_sequence, pixels))
+
+    faces = np.asarray(faces)
+    faces = np.expand_dims(faces, -1)
+    emotions = pd.get_dummies(data['emotion']).to_numpy()
+    return faces, emotions
 
 def preprocess_input(x, v2=True):
     x = x.astype('float32')
